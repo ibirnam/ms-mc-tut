4d670f662b6608046245d507eff23f22a932e40d
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-01-17 10:04:50 -0800

    chapter 6 done

--- public/index.js
+++ public/index.js
@@ -4,6 +4,8 @@ $(document).ready(() => {
 
     //Keep track of the current user
     let currentUser;
+    // Get the online users from the server
+    socket.emit('get online users');
 
     $('#createUserBtn').click((e) => {
         e.preventDefault();
@@ -46,5 +48,21 @@ $(document).ready(() => {
             </div>
         `);
     })
+
+    socket.on('get online users', (onlineUsers) => {
+        //You may have not have seen this for loop before. It's syntax is for(key in obj)
+        //Our usernames are keys in the object of onlineUsers.
+        for (username in onlineUsers) {
+            $('.usersOnline').append(`<p class="userOnline">${username}</p>`);
+        }
+    })
+
+    //Refresh the online user list
+    socket.on('user has left', (onlineUsers) => {
+        $('.usersOnline').empty();
+        for (username in onlineUsers) {
+            $('.usersOnline').append(`<p>${username}</p>`);
+        }
+    });
 
 })
