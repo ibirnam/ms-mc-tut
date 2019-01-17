42249106ac32c5b7ff2368035137d823bcbc783d
Ian Birnam <ianbirnam@Ians-MacBook-Pro.local>
2019-01-17 09:35:44 -0800

    chapter 4 done

--- public/index.js
+++ public/index.js
@@ -1,4 +1,21 @@
+// index.js
 $(document).ready(() => {
-    //Connect to the socket.io server
+
     const socket = io.connect();
+
+    $('#createUserBtn').click((e) => {
+        e.preventDefault();
+        let username = $('#usernameInput').val();
+        if (username.length > 0) {
+            //Emit to the server the new user
+            socket.emit('new user', username);
+            $('.usernameForm').remove();
+        }
+    });
+
+    //socket listeners
+    socket.on('new user', (username) => {
+        console.log(`✋ ${username} has joined the chat! ✋`);
+    })
+
 })
